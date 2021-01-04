f1df7089b70c0b40e8be7df44563fcefac7ad85f
benhassenwael <greenw2012@gmail.com>
2021-01-04 20:10:18 +0100

    Task: 100-101-102

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+    except:
+        print("Exception:", sys.exc_info()[1], file=sys.stderr)
+        return None
+    return res
