033900001ca57c343129d443532dc8b558dc2f3a
benhassenwael <greenw2012@gmail.com>
2021-01-20 21:39:39 +0100

    Fix: 101 sorted print

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -21,15 +21,12 @@ status = {
         "405": 0,
         "500": 0
          }
-i = 0
 try:
-    for line in sys.stdin:
-        line = line.strip("\n").split(" ")
+    for i, line in enumerate(sys.stdin, 1):
+        line = line.split()
         file_size += int(line[-1])
         status[line[-2]] += 1
-        i += 1
-        if i == 10:
-            i = 0
+        if i % 10 == 0:
             print_status(file_size, status)
 except KeyboardInterrupt:
     print_status(file_size, status)
