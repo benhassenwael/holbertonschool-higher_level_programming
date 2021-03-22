27140fafe8e01ae619dfe9d43648cef91b30d298
benhassenwael <greenw2012@gmail.com>
2021-03-22 19:48:36 +0100

    fix typo

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 langs.forEach(function (item) { console.log(item); });
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const multi = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const multi = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const item in multi) {
   console.log(multi[item]);
 }
